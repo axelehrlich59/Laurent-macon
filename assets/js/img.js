16e464a2318b95928fb9009a3e3f462834537e67
axelehrlich59 <53488067+axelehrlich59@users.noreply.github.com>
2020-04-23 17:58:11 +0200

    try to add animations to realisations photo

---
+++ assets/js/img.js
@@ -0,0 +1,27 @@
+
+//Code for realisation pictures
+
+
+/*
+var largeImage = document.getElementsByName('img');
+
+function swipe() {
+
+largeImage[i].style.display = 'block';
+largeImage[i].style.width = 1000+"px";
+largeImage[i].style.height = 100+"%";
+
+var url= largeImage.getAttribute('src');
+window.open(url,'Image','width=largeImage.style.width,height=largeImage.style.height,resizable=1');
+
+}
+
+for(var i = 0; i < largeImage.length ; i++) {
+if(largeImage.length) {
+
+
+largeImage[i].addEventListener('click',swipe(),false);
+}
+}
+
+*/
